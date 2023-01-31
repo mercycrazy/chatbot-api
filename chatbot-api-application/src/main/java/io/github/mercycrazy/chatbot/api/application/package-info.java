0c3ad26fa0ac497a39e1c01dd59fd4d37c0d8c82
mercycrazy <1443424326@qq.com>
2023-02-01 00:43:38 +0800

    feat: init ddd project

---
+++ chatbot-api-application/src/main/java/io/github/mercycrazy/chatbot/api/application/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * TODO Comment
+ *
+ * @author <a href="mailto:1443424326@qq.com">mercycrazy</a>
+ * @since 2023-02-01
+ */
+package io.github.mercycrazy.chatbot.api.application;
---
+++ chatbot-api-interfaces/src/main/java/io/github/mercycrazy/chatbot/api/ApiApplication.java
@@ -0,0 +1,19 @@
+package io.github.mercycrazy.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ *
+ * @author <a href="mailto:1443424326@qq.com">mercycrazy</a>
+ * @since 2023-02-01
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
