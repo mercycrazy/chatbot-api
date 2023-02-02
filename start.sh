ac1d0b6df0ec7219c1ecbc22c6432b86d024fa79
mercycrazy <1443424326@qq.com>
2023-02-03 00:29:15 +0800

    fix: 修复启动脚本逻辑

--- start.sh
+++ start.sh
@@ -1,20 +1,17 @@
 IMAGE_TAG="1.0"
 APP_NAME="chatbot-api"
+IMAGE_NAME="mercycrazy/${APP_NAME}:${IMAGE_TAG}"
 
 # 构建可执行 Jar
 mvn clean package
 
 # 打包镜像
-docker build -f ./Dockerfile -t mercycrazy/chatbot-api:${IMAGE_TAG} .
+docker build -f ./Dockerfile -t ${IMAGE_NAME} .
 
 # 上传镜像
 echo aa677877+ | docker login --password-stdin -u mercycrazy
-docker push mercycrazy/chatbot-api:${IMAGE_TAG}
+docker push ${IMAGE_NAME}
 
-CONTAINER_ID=$(docker ps -a | grep ${APP_NAME} | grep -v grep | awk '{print $1}')
-if [ ! "$CONTAINER_ID" ]; then
-  docker rm -f $APP_NAME
-fi
-
-ssh root@124.220.158.175 "docker run -p 8090:8090 --name ${APP_NAME} -d ${APP_NAME}:${IMAGE_TAG}"
+# 启动服务
+ssh root@124.220.158.175 "docker rm -f ${APP_NAME}; docker run -p 8090:8090 --name ${APP_NAME} -d ${IMAGE_NAME}"
 
